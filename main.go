0ad2dbc669050761914ea5a831e5f8a6ad0a8427
fanice1998 <fanice0721@gmail.com>
2023-12-30 18:58:58 +0800

    try get binance klins day03

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func init() {
 func main() {
 	var client tools.Client
 	client.BaseURL = BaseURL
+	client.symbol = "ETHUSDT"
 
 	tools.HistoricalPrice(client)
 }
--- tools/history.go
+++ tools/history.go
@@ -6,36 +6,48 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strings"
 )
 
 type Client struct {
-	BaseURL string
+	BaseURL  string
+	symbol   string
+	interval string
 }
 
 func HistoricalPrice(client Client) {
 	//url := "https://data.binance.vision/?prefix=data/futures/um/monthly/klines/ETHUSDT/1h/"
-	//xml := "https://s3-ap-northeast-1.amazonaws.com/data.binance.vision?delimiter=/&prefix=data/futures/"
 	fmt.Println("Get URL string: ", client.BaseURL)
 
 	// 返回合約交易所有USDT交易對 symbol
-	parser := xmlParser("data/futures/um/monthly/klines/ETHUSDT/")
+	// 取得xml 標籤結果
+	var parser []string
+	prefix := "data/futures/um/monthly/klines"
+	if client.interval != "" && client.symbol != "" {
+		// 取得歷史數據路徑
+		parser = xmlParser(fmt.Sprintf("%s/%s/%s/", prefix, client.symbol, client.interval))
+	} else if client.interval == "" && client.symbol != "" {
+		// 取得時間軸分類
+		parser = xmlParser(fmt.Sprintf("%s/%s/", prefix, client.symbol))
+	} else if client.symbol == "" {
+		// 取得symbols
+		parser = xmlParser(prefix + "/")
+	} else {
+		// 發生錯誤
+		parser = append(parser, "Error message: client class configuration error.")
+	}
 	fmt.Println(parser)
 }
 
-func xmlParser(prefix string) map[string]string {
-	result := make(map[string]string)
+func xmlParser(prefix string) []string {
+	result := make([]string, 0, 150)
 	xmlurl := "https://s3-ap-northeast-1.amazonaws.com/data.binance.vision?delimiter=/&prefix=" + prefix
 	fmt.Println("Xml url is ", xmlurl)
 
-	lastString := strings.Split(xmlurl, "/")[len(strings.Split(xmlurl, "/"))-2]
-
-	fmt.Println(lastString)
+	// 請求url
 	req, err := http.NewRequest("GET", xmlurl, nil)
 	if err != nil {
 		log.Println(err)
 	}
-
 	conn := &http.Client{}
 	resp, err := conn.Do(req)
 	if err != nil {
@@ -43,6 +55,7 @@ func xmlParser(prefix string) map[string]string {
 	}
 	defer resp.Body.Close()
 
+	// 解析xml響應
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
@@ -53,43 +66,38 @@ func xmlParser(prefix string) map[string]string {
 	content := new(ListBuckResult)
 	err = xml.Unmarshal(body, content)
 
-	// 為了方便可視化結果
-	switch {
-	case lastString == "klines":
-		// 只記錄使用USDT的交易對
-		// Only record trading pairs using USDT.
-		lenP := len(prefix) // prefix lens
-		for _, v := range content.CommonPrefixes {
-			if v.Prefix[len(v.Prefix)-5:len(v.Prefix)-1] == "USDT" {
-				result[v.Prefix[lenP:len(v.Prefix)-1]] = v.Prefix
-			}
+	// 取得xml標籤內容
+	if len(content.Contents) != 0 {
+		// 取得路徑
+		for _, v := range content.Contents {
+			result = append(result, v.Key)
+			log.Println("content.Contents", v)
 		}
-		return result
-
-	case lastString[len(lastString)-4:] == "USDT":
+	} else if len(content.Prefix) != 0 {
+		// 取得檔案名稱
 		for _, v := range content.CommonPrefixes {
-			lastS := strings.LastIndex(v.Prefix, "/")
-			fmt.Println(v.Prefix[lastS:])
-			result["intervar"] += v.Prefix + "/"
+			result = append(result, v.Prefix)
+			log.Println("content.Prefix", v)
 		}
-		return result
-
-	default:
-		// 未搜尋到匹配的類別
-		//result["err"] = "No matching category found! "
-		for _, v := range content.CommonPrefixes {
-			result[v.Prefix] = v.Prefix
-		}
-		return result
+	} else {
+		// 未搜尋到匹配的項目
+		result = append(result, "Error message: not matching category found.")
 	}
+	return result
 
 }
 
 type ListBuckResult struct {
+	// xml 格式
 	Prefix         string           `xml:"Prefix"`
 	CommonPrefixes []CommonPrefixes `xml:"CommonPrefixes"`
+	Contents       []Contents       `xml:"Contents"`
 }
 
 type CommonPrefixes struct {
 	Prefix string `xml:"Prefix"`
+}
+
+type Contents struct {
+	Key string `xml:"Key"`
 }
